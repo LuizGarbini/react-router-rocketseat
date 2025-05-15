7d4c5346f70b5d2bc6bef3bea7a655294a7afcf8
LuizGarbini <garbasneto@gmail.com>
2025-05-14 23:59:55 -0300

    Layout Routes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,4 +1,6 @@
 import { Route, Routes } from "react-router";
+
+import { Layout } from "../layout";
 import { Details } from "../pages/Details";
 import { Home } from "../pages/Home";
 import { NotFound } from "../pages/NotFound";
@@ -7,9 +9,11 @@ import { Products } from "../pages/Products";
 export function AppRoutes() {
 	return (
 		<Routes>
-			<Route path="/" index element={<Home />} />
-			<Route path="/products" element={<Products />} />
-			<Route path="/details/:id" element={<Details />} />
+			<Route path="/" element={<Layout />}>
+				<Route path="/" index element={<Home />} />
+				<Route path="/products" element={<Products />} />
+				<Route path="/details/:id" element={<Details />} />
+			</Route>
 
 			<Route path="*" element={<NotFound />} />
 		</Routes>
