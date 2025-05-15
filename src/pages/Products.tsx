62361af7a6870477f490e7d1796a6657c2bcddb0
LuizGarbini <garbasneto@gmail.com>
2025-05-14 23:50:47 -0300

    useParams

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -15,6 +15,14 @@ export function Products() {
 					Categoria <strong>{category}</strong>
 				</span>
 			)}
+
+			<div className="cards">
+				<a href="/details/1">Produtos 1</a>
+				<a href="/details/2">Produtos 2</a>
+				<a href="/details/3">Produtos 3</a>
+				<a href="/details/4">Produtos 4</a>
+				<a href="/details/5">Produtos 5</a>
+			</div>
 		</div>
 	);
 }
