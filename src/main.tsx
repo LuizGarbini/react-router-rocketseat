f5f28eebc1cd18b89b24e09142cea3e531ba987c
LuizGarbini <garbasneto@gmail.com>
2025-05-14 23:08:30 -0300

    Criando a Primeira Rota

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
 
-import './main.css'
-import { App } from './App' 
+import "./main.css";
+import { AppRoutes } from "./routes/AppRoutes";
 
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<BrowserRouter>
+			<AppRoutes />
+		</BrowserRouter>
+	</StrictMode>,
+);
